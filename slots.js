a365708baec8417a6e96b7649819a43e658a7e54
l3wk3m <>
2024-01-22 03:00:16 +0000

    Feat: Added win and lose conditions

--- slots.js
+++ slots.js
@@ -8,6 +8,31 @@ let stopPosition = ['???', '???', '???'];
 let credit = parseInt(document.getElementById('credit-counter').innerHTML);
 let bet = parseInt(document.getElementById('bet-tally').innerHTML);
 
+// Declaring variable to help navigate the DOM
+const spinButton = document.getElementById('spin-button');
+const increment = document.getElementById('more');
+const decrement = document.getElementById('less');
+
+
+// Function for incrementing or decrementing the bet amount by 1 for each click of an arrow beside the counter
+function more(event) {
+    bet += 1;
+    document.getElementById('bet-tally').innerHTML = bet;
+}
+
+function less(event) {
+    bet -= 1;
+    document.getElementById('bet-tally').innerHTML = bet;
+}
+
+
+// Code for storing and updating credit values and pushing
+function wager(event) {
+    credit = credit - bet;
+    document.getElementById('credit-counter').innerHTML = credit;
+}
+
+
 // Slot machine spin and result code
 
 function spin(event) {
@@ -26,31 +51,44 @@ function spin(event) {
   console.log(stopPosition);
 }
 
-const spinButton = document.getElementById('spin-button');
-const increment = document.getElementById('more');
-const decrement = document.getElementById('less');
+// Win Condition Code
 
-// Function for incrementing or decrementing the bet amount by 1 for each click of an arrow beside the counter
-function more(event) {
-  bet += 1;
-  document.getElementById('bet-tally').innerHTML = bet;
+function winCheck(event){
+    if (stopPosition[0] === stopPosition[1]) {
+        console.log('You win!');
+        credit = credit + (bet * 3);
+        document.getElementById('credit-counter').innerHTML = credit;
+    } else if (stopPosition[0] === stopPosition[2]) {
+        console.log('You win!');
+        credit = credit + (bet * 3);
+        document.getElementById('credit-counter').innerHTML = credit;
+    } else if (stopPosition[1] === stopPosition[2]) {
+        console.log('You win!');
+        credit = credit + (bet * 3);
+        document.getElementById('credit-counter').innerHTML = credit;
+    } else if (stopPosition[0] === stopPosition[1] === stopPosition[2]) {
+        console.log('!!!YOU WIN BIG!!!');
+        credit = credit + (bet * 10);
+        document.getElementById('credit-counter').innerHTML = credit;
+    } else {
+        console.log('Hard luck - try again!');
+    }
 }
 
-function less(event) {
-  bet -= 1;
-  document.getElementById('bet-tally').innerHTML = bet;
+function loseCheck(event) {
+    if (credit = 0) {
+        console.log("You're outta cash - GAME OVER!")
+    } else if (credit < 0) {
+        console.log("You've dropped into debt - GAME OVER!")
+    }
 }
 
-
-// Code for storing and updating credit values and pushing
-function wager(event) {
-  credit = credit - bet;
-  document.getElementById('credit-counter').innerHTML = credit;
-}
-
-// Spin button event listener
-spinButton.addEventListener('click', spin);
-spinButton.addEventListener('click', wager);
 // Bet change event listeners
 increment.addEventListener('click', more);
 decrement.addEventListener('click', less);
+
+// Spin button event listener
+spinButton.addEventListener('click', wager);
+spinButton.addEventListener('click', spin);
+spinButton.addEventListener('click', winCheck);
+spinButton.addEventListener('click', loseCheck);
