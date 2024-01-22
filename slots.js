2eec3c534b64e4ffb9851b32facefb1c223861e2
l3wk3m <>
2024-01-22 02:32:25 +0000

    Feat: Added credit score tracker, bet amount tracker and DOM manipulation for each of these to update the user on their bet amounts.

--- slots.js
+++ slots.js
@@ -5,8 +5,8 @@ const reel = ['lemon', 'melon', 'seven', 'cherry', 'passionfruit', 'bell', 'cher
 let stopPosition = ['???', '???', '???'];
 
 // Declaring variables for 'credit storage' and 'bets'
-let credit = 100;
-let bet = 10;
+let credit = parseInt(document.getElementById('credit-counter').innerHTML);
+let bet = parseInt(document.getElementById('bet-tally').innerHTML);
 
 // Slot machine spin and result code
 
@@ -27,9 +27,30 @@ function spin(event) {
 }
 
 const spinButton = document.getElementById('spin-button');
+const increment = document.getElementById('more');
+const decrement = document.getElementById('less');
 
-spinButton.addEventListener('click', spin);
+// Function for incrementing or decrementing the bet amount by 1 for each click of an arrow beside the counter
+function more(event) {
+  bet += 1;
+  document.getElementById('bet-tally').innerHTML = bet;
+}
+
+function less(event) {
+  bet -= 1;
+  document.getElementById('bet-tally').innerHTML = bet;
+}
 
 
 // Code for storing and updating credit values and pushing
+function wager(event) {
+  credit = credit - bet;
+  document.getElementById('credit-counter').innerHTML = credit;
+}
 
+// Spin button event listener
+spinButton.addEventListener('click', spin);
+spinButton.addEventListener('click', wager);
+// Bet change event listeners
+increment.addEventListener('click', more);
+decrement.addEventListener('click', less);
