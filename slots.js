4b36aa912a927baf4ab3081ce8be7655059f50c2
l3wk3m <>
2024-01-22 01:44:41 +0000

    Feat: Added js code to log slot reel stop point to the console. Added variable for holding credit and bet info to js code. Added divs to the footer for representing these values to the index.html. Properly linked stylesheet.

--- slots.js
+++ slots.js
@@ -1,11 +1,26 @@
-let stopPosition = [0, 0, 0];
+// Declaring variables for the 'spin()' function
+let stopValue = [0, 0, 0];
 let newValue = [];
+const reel = ['lemon', 'melon', 'seven', 'cherry', 'passionfruit', 'bell', 'cherries', 'orange', 'roulette'];
+let stopPosition = ['???', '???', '???'];
+
+// Declaring variables for 'credit storage' and 'bets'
+let credit = 100;
+let bet = 10;
+
+// Slot machine spin and result code
 
 function spin(event) {
   
   for (i = 0; i < 3; i++) {
     newValue[i] = ((Math.round(Math.random() * 10)) % 9);
-    stopPosition.splice(i, 1, newValue[i]);
+    stopValue.splice(i, 1, newValue[i]);
+  }
+  
+  console.log(stopValue);
+
+  for (i = 0; i < 3; i++) {
+    stopPosition.splice(i, 1, reel[stopValue[i]]);
   }
 
   console.log(stopPosition);
@@ -14,3 +29,7 @@ function spin(event) {
 const spinButton = document.getElementById('spin-button');
 
 spinButton.addEventListener('click', spin);
+
+
+// Code for storing and updating credit values and pushing
+
